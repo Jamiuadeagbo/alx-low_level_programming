54d06f9a1f992b557707a9ac495453412f309a0e
jamiuadeagbo <jamiuadeagbo@gmail.com>
2022-12-09 10:21:06 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - main block
+ * main - entry point
+ *
  * return: #
  */
+
 int main<void>
 {
-	puts(*\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
