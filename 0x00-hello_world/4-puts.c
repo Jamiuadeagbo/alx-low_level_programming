921ae4da4595b08aac4aa8f5e82a8f992f813db0
jamiuadeagbo <jamiuadeagbo@gmail.com>
2022-12-12 08:23:19 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * Main - Entry Point
  *
- * return: #
+ * Return: Always 0 (Success)
  */
 
-int main<void>
+int main(void)
+
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts(*\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
